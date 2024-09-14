c2dacc6f1b396d68478295d27ac3b7a525e1a1cc
Matt Burkhard <60051007+umatt1@users.noreply.github.com>
2024-09-13 23:11:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,9 @@
 # flask
 from flask import Flask, render_template, Blueprint
 from chess import openings_guru
-from twitter import synthetic_parrot
-from covid_simulator import covid_simulator
-from motif_finder import motifer
 
 app = Flask(__name__)
 app.register_blueprint(openings_guru, url_prefix="/chess")
-app.register_blueprint(synthetic_parrot)
-app.register_blueprint(covid_simulator)
-app.register_blueprint(motifer, url_prefix="/motif_finder")
 
 @app.route("/")
 def home():
